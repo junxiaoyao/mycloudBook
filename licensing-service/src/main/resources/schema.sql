8a69872df03fa337c25b3c3db359892c1435c9f0
ybl <1162474984@qq.com>
2019-05-29 14:14:49 +0800

    spring-cloud配置中心,git方式配置

--- licensing-service/src/main/resources/schema.sql
+++ licensing-service/src/main/resources/schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS licenses;
+DROP TABLE IF EXISTS licensx;
 
-CREATE TABLE licenses (
+CREATE TABLE licensx (
   id integer PRIMARY KEY,
   license_id        VARCHAR(100)  NOT NULL,
   organization_id   TEXT NOT NULL,
@@ -11,11 +11,11 @@ CREATE TABLE licenses (
   comment           VARCHAR(100));
 
 
-INSERT INTO licenses (id,license_id,  organization_id, license_type, product_name, license_max, license_allocated)
+INSERT INTO licensx (id,license_id,  organization_id, license_type, product_name, license_max, license_allocated)
 VALUES (1,'f3831f8c-c338-4ebe-a82a-e2fc1d1ff78a', 'e254f8c-c442-4ebe-a82a-e2fc1d1ff78a', 'user','customer-crm-co', 100,5);
-INSERT INTO licenses (id,license_id,  organization_id, license_type, product_name, license_max, license_allocated)
+INSERT INTO licensx (id,license_id,  organization_id, license_type, product_name, license_max, license_allocated)
 VALUES (2,'t9876f8c-c338-4abc-zf6a-ttt1', 'e254f8c-c442-4ebe-a82a-e2fc1d1ff78a', 'user','suitability-plus', 200,189);
-INSERT INTO licenses (id,license_id,  organization_id, license_type, product_name, license_max, license_allocated)
+INSERT INTO licensx (id,license_id,  organization_id, license_type, product_name, license_max, license_allocated)
 VALUES (3,'38777179-7094-4200-9d61-edb101c6ea84', '442adb6e-fa58-47f3-9ca2-ed1fecdfe86c', 'user','HR-PowerSuite', 100,4);
-INSERT INTO licenses (id,license_id,  organization_id, license_type, product_name, license_max, license_allocated)
+INSERT INTO licensx (id,license_id,  organization_id, license_type, product_name, license_max, license_allocated)
 VALUES (4,'08dbe05-606e-4dad-9d33-90ef10e334f9', '442adb6e-fa58-47f3-9ca2-ed1fecdfe86c', 'core-prod','WildCat Application Gateway', 16,16);
