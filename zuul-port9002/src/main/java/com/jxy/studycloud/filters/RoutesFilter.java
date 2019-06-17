309580e6246c08cc1fc7fb4c7ff68184a370d0f1
junxiaoyao <1162474984@qq.com>
2019-06-17 21:48:05 +0800

    rest

--- zuul-port9002/src/main/java/com/jxy/studycloud/filters/RoutesFilter.java
+++ zuul-port9002/src/main/java/com/jxy/studycloud/filters/RoutesFilter.java
@@ -3,9 +3,34 @@ package com.jxy.studycloud.filters;
 import com.jxy.studycloud.model.AbTestingRoute;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
+import org.apache.http.Header;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPatch;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.InputStreamEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicHttpRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.zuul.filters.ProxyRequestHelper;
 import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
 import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @description
@@ -15,36 +40,158 @@ import org.springframework.stereotype.Component;
 @Component
 public class RoutesFilter extends ZuulFilter {
 
-  private static final int FILTR_ORDER = 1;
-  private static final boolean SHOULD_FILTER = true;
-  @Autowired
-  private FilterUtils filterUtils;
+    private static final int FILTR_ORDER = 1;
+    private static final boolean SHOULD_FILTER = true;
+    @Autowired
+    private FilterUtils filterUtils;
+    private ProxyRequestHelper helper = new ProxyRequestHelper();
 
-  @Override
-  public String filterType() {
-    return FilterUtils.ROUTE_FILTER_TYPE;
-  }
+    @Override
+    public String filterType() {
+        return FilterUtils.ROUTE_FILTER_TYPE;
+    }
 
-  @Override
-  public int filterOrder() {
-    return FILTR_ORDER;
-  }
+    @Override
+    public int filterOrder() {
+        return FILTR_ORDER;
+    }
 
-  @Override
-  public boolean shouldFilter() {
-    return SHOULD_FILTER;
-  }
+    @Override
+    public boolean shouldFilter() {
+        return SHOULD_FILTER;
+    }
 
-  @Override
-  public Object run() {
-    RequestContext requestContext = RequestContext.getCurrentContext();
-    System.out.println(filterUtils.getServiceId());
-    return null;
-  }
+    @Override
+    public Object run() {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        System.out.println(filterUtils.getServiceId());
+        return null;
+    }
+
+    private void forworadToSpecialRouter(String route) {
+        RequestContext requestContext = RequestContext.getCurrentContext();
+        HttpServletRequest request = requestContext.getRequest();
+        MultiValueMap<String, String> headers = helper.buildZuulRequestHeaders(request);
+        MultiValueMap<String, String> params = helper.buildZuulRequestQueryParams(request);
+        String verb = getVerb(request);
+        InputStream requestEntity = getRequestBody(request);
+        if (request.getContentLength() < 0)
+            requestContext.setChunkedRequestBody();
+        this.helper.addIgnoredHeaders();
+        CloseableHttpClient httpClient = null;
+        HttpResponse response = null;
+        try {
+            httpClient = HttpClients.createDefault();
+           httpClient  = HttpClients.createDefault();
+            response = forward(httpClient, verb, route, request, headers,
+                    params, requestEntity);
+            setResponse(response);
+        } catch (Exception e) {
+
+        }
+    }
+
+    private InputStream getRequestBody(HttpServletRequest request) {
+        InputStream requestEntity = null;
+        try {
+            requestEntity = request.getInputStream();
+        } catch (IOException ex) {
+            // no requestBody is ok.
+        }
+        return requestEntity;
+    }
+
+    private void setResponse(HttpResponse response) throws IOException {
+        this.helper.setResponse(response.getStatusLine().getStatusCode(),
+                response.getEntity() == null ? null : response.getEntity().getContent(),
+                revertHeaders(response.getAllHeaders()));
+    }
+
+    private HttpResponse forward(HttpClient httpclient, String verb, String uri,
+                                 HttpServletRequest request, MultiValueMap<String, String> headers,
+                                 MultiValueMap<String, String> params, InputStream requestEntity)
+            throws Exception {
+        Map<String, Object> info = this.helper.debug(verb, uri, headers, params,
+                requestEntity);
+        URL host = new URL(uri);
+        HttpHost httpHost = getHttpHost(host);
+
+        HttpRequest httpRequest;
+        int contentLength = request.getContentLength();
+        InputStreamEntity entity = new InputStreamEntity(requestEntity, contentLength,
+                request.getContentType() != null
+                        ? ContentType.create(request.getContentType()) : null);
+        switch (verb.toUpperCase()) {
+            case "POST":
+                HttpPost httpPost = new HttpPost(uri);
+                httpRequest = httpPost;
+                httpPost.setEntity(entity);
+                break;
+            case "PUT":
+                HttpPut httpPut = new HttpPut(uri);
+                httpRequest = httpPut;
+                httpPut.setEntity(entity);
+                break;
+            case "PATCH":
+                HttpPatch httpPatch = new HttpPatch(uri);
+                httpRequest = httpPatch;
+                httpPatch.setEntity(entity);
+                break;
+            default:
+                httpRequest = new BasicHttpRequest(verb, uri);
+
+        }
+        try {
+            httpRequest.setHeaders(convertHeaders(headers));
+            HttpResponse zuulResponse = forwardRequest(httpclient, httpHost, httpRequest);
+
+            return zuulResponse;
+        } finally {
+        }
+    }
 
 
-  public boolean useSpecialRoute() {
-    int num = (int) (Math.random() * 10);
-    return num>5;
-  }
+    private HttpResponse forwardRequest(HttpClient httpclient, HttpHost httpHost,
+                                        HttpRequest httpRequest) throws IOException {
+        return httpclient.execute(httpHost, httpRequest);
+    }
+
+    private MultiValueMap<String, String> revertHeaders(Header[] headers) {
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<String, String>();
+        for (Header header : headers) {
+            String name = header.getName();
+            if (!map.containsKey(name)) {
+                map.put(name, new ArrayList<String>());
+            }
+            map.get(name).add(header.getValue());
+        }
+        return map;
+    }
+
+    public boolean useSpecialRoute() {
+        int num = (int) (Math.random() * 10);
+        return num > 5;
+    }
+
+    private String getVerb(HttpServletRequest request) {
+        String sMethod = request.getMethod();
+        return sMethod.toUpperCase();
+    }
+
+    private HttpHost getHttpHost(URL host) {
+        HttpHost httpHost = new HttpHost(host.getHost(), host.getPort(),
+                host.getProtocol());
+        return httpHost;
+    }
+
+    private Header[] convertHeaders(MultiValueMap<String, String> headers) {
+        List<Header> list = new ArrayList<>();
+        for (String name : headers.keySet()) {
+            for (String value : headers.get(name)) {
+                list.add(new BasicHeader(name, value));
+            }
+        }
+        return list.toArray(new BasicHeader[0]);
+    }
+
 }
